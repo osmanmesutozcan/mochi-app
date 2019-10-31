30ba214d103de4dcc79448930c5eb930fb766137
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2019-11-01 00:18:41 +0800

    move some styles

--- src/application-extension/index.tsx
+++ src/application-extension/index.tsx
@@ -5,16 +5,15 @@ import { MochiShell } from '../application/shell';
  * The default JupyterLab application shell.
  */
 const shell: MochiFrontEndPlugin<IMochiShell> = {
-  id: '@jupyterlab/application-extension:shell',
+  id: '@mochi/application-extension:shell',
+  autoStart: true,
+  provides: IMochiShell,
   activate: (app: MochiFrontEnd) => {
-    console.log('started');
     if (!(app.shell instanceof MochiShell)) {
       throw new Error(`${shell.id} did not find a LabShell instance.`);
     }
     return app.shell;
   },
-  autoStart: true,
-  provides: IMochiShell,
 };
 
 const plugins: MochiFrontEndPlugin<any>[] = [shell];
