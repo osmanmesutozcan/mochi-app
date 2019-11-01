157d0d4e8810982036b824948662575b561d1fdc
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2019-11-01 14:38:35 +0800

    WIP database service structure

---
+++ src/services/database/connector.ts
@@ -0,0 +1,12 @@
+/**
+ * A common database client wrapper to
+ * communicate with different types of underlying
+ * database connections.
+ */
+export class Database {
+  //
+}
+
+export namespace Database {
+  //
+}
